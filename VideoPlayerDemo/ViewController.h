8370bb02479db6cfa558d73bbea6d7cc5ad806e2
je_ffy <jzxiaobao@gmail.com>
2016-10-16 15:55:23 +0800

    Initial Commit

---
+++ VideoPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPlayerDemo
+//
+//  Created by je_ffy on 2016/10/16.
+//  Copyright © 2016年 je_ffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
